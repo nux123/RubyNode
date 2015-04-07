56bcbd328f04cb81c6827a6a74680e9483025a03
nux123 <nux233@163.com>
2015-04-07 10:08:03 +0800

    first

---
+++ beginEndTest.rb
@@ -0,0 +1,9 @@
+puts "that will be second"
+
+BEGIN{
+    puts "this is a begin test, it will first"
+}
+
+END{
+	puts "tihs will be last"
+}
---
+++ first.rb
@@ -0,0 +1,6 @@
+print <<"FOO",<<"EOO"
+   I wanna be a ruby Programmer
+   Hello Ruby
+FOO
+   HELLO WORLD 
+EOO
