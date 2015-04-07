b59521486c0c4956f9eee512a9e74eebc4882cb2
nux123 <nux233@163.com>
2015-04-07 10:29:42 +0800

    增加注释

--- beginEndTest.rb
+++ beginEndTest.rb
@@ -1,3 +1,9 @@
+=begin
+begin代码块和end代码块
+begin在程序开始时执行
+end在程序结束时执行
+=end
+
 puts "that will be second"
 
 BEGIN{
--- first.rb
+++ first.rb
@@ -1,3 +1,9 @@
+=begin
+   终止符<<EOF
+   在程序遇到EOF时结束
+   也可以堆叠使用 
+=end
+
 print <<"FOO",<<"EOO"
    I wanna be a ruby Programmer
    Hello Ruby
